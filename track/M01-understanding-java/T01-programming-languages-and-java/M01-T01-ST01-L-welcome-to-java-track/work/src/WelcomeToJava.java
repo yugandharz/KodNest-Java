604820f8e852fccefb68b4c2a7473dbd265d1798
yugandharz <pyugandhar176@gmail.com>
2026-09-02 10:55:05 +0530

    this is my start in java

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
+       System.out.println("welcome to java");
+       System.out.println("i am learning java");
     }
 }
