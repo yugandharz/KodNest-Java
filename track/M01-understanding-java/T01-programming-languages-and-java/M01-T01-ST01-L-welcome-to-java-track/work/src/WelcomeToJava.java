1f9931db16148592b1e1889dffd8a0403cc203a0
yugandharz <pyugandhar176@gmail.com>
2026-09-02 17:57:22 +0530

    revise

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,5 +3,6 @@ public class WelcomeToJava {
        //Write your code here
        System.out.println("welcome to java");
        System.out.println("i am learning java");
+       System.out.println("i am from andhrapradesh");
     }
 }
