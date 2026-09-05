270d815d736a851a8999bf694f5ed15810911140
yugandharz <pyugandhar176@gmail.com>
2026-09-05 13:50:48 +0530

    Display Repository Status

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/DisplayRepositoryStatus.java
@@ -0,0 +1,9 @@
+public class DisplayRepositoryStatus{
+    public static void main(String[] args){
+        // Display the repository cloning status.
+        System.out.println("Repository: Cloned Successfully");
+
+        // Display whether the project is ready.
+        System.out.println("Project Status: Ready for Development");
+    }
+}
