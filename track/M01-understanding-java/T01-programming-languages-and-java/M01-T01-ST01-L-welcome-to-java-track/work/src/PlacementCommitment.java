b6f9f671e1721227e41b9b98876774a444cd47ea
yugandharz <pyugandhar176@gmail.com>
2026-09-05 13:15:29 +0530

    my commitment

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/PlacementCommitment.java
@@ -0,0 +1,8 @@
+public class PlacementCommitment{
+    public static void main(String[] args){
+        System.out.println("---MY PLACEMENT GOAL ---");
+        System.out.println("ROLE: Software Developer");
+        System.out.println("Target: Get Placed");
+        System.out.println("Daily practice: 8 Hours");
+    }
+}
