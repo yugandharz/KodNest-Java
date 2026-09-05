a2e539e9df19cca970799a9f82a425123613082b
yugandharz <pyugandhar176@gmail.com>
2026-09-05 14:33:41 +0530

    gg

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/StudentidCard.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/StudentidCard.java
@@ -1,5 +1,5 @@
 public class StudentidCard{
-    public static void main(String args){
+    public static void main(String[] args){
         System.out.println("----------------------");
         System.out.println("STUDENT PROFILE");
         System.out.println("Name: Alex");
