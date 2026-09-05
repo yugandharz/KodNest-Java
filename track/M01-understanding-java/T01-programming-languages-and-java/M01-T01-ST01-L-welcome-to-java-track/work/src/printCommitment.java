e01f5464dceb24ab8b329555edb0363e00211b5f
yugandharz <pyugandhar176@gmail.com>
2026-09-05 12:56:57 +0530

    printing commitment of my learning

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/printCommitment.java
@@ -0,0 +1,8 @@
+public class printCommitment{
+    public static void main(String[] args){
+        // Display the student's learning track.
+        System.out.println("Track: Java Development");
+        // Display the student's daily commitment.
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
