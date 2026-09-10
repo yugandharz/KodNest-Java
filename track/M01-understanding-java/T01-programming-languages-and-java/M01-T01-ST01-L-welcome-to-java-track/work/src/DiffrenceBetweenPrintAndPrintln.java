eb654ae5762f196ad7ea357d28777caebdd773a1
yugandharz <pyugandhar176@gmail.com>
2026-09-10 14:00:53 +0530

    diffrence between print and println

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/DiffrenceBetweenPrintAndPrintln.java
@@ -0,0 +1,18 @@
+public class DiffrenceBetweenPrintAndPrintln {
+    public static void main (String[] args){
+        System.out.print("Hello ");
+        System.out.println("world");
+        //output = Hello World
+        System.out.println("my name is");
+        System.out.print("yugandhar ");
+        /*output = my name is
+                   yugandhar */
+        System.out.println("pujari");
+        System.out.println("i want to be a java developer");
+        System.out.println ("present i am in kodnest ");
+        /*output = i want to be a java developer
+                   present i am in kodnest */    
+        
+    }
+    
+}
