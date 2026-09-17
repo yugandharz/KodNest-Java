8581fd58850cd0092dc9e0bd55dd53c0fa2fc79e
yugandharz <pyugandhar176@gmail.com>
2026-09-17 20:14:15 +0530

    done

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/StudentMarks.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/StudentMarks.java
@@ -10,7 +10,11 @@ public class StudentMarks {
                         (marks[4] >= 0 && marks[4] <=100);
         
         String result = Percentage < 40 ? "fail" : Percentage < 60? "pass" : Percentage < 75? "first class" : "Distinction";
-        String Scholorship = (Percentage >= 85 && marks[0] >= 75 && marks[1] >=75 && marks[2] >= 75 && marks[3] >= 75 && marks[4] >= 75 )? "eligible" : "not eligible";
-
+        String Scholarship = (Percentage >= 85 && marks[0] >= 75 && marks[1] >=75 && marks[2] >= 75 && marks[3] >= 75 && marks[4] >= 75 )? "eligible" : "not eligible";
+        System.out.println("Percentage: " + Percentage);
+        System.out.println("Total Marks: " + totalmarks);
+        System.out.println("Vaild Marks: " + vaild);
+        System.out.println("Result: " + result);
+        System.out.println("Scholarship: " + Scholarship);
     }
 }
